8d4b8fa2e14306d86362cf1ec19e4d1eab8e81f3
Nikita Borodikhin <eliterr@gmail.com>
2026-07-09 13:20:51 -0500

    1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.easynap"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1003
-        versionName = "1.3"
+        versionCode = 1004
+        versionName = "1.4"
         testInstrumentationRunner = "me.easynap.HiltTestRunner"
     }
 
